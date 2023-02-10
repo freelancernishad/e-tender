d468b95f6f48bcf37ddd5b58ebb8563c0b1e64f1
Softlabltd.com <freelancernishad123@gmail.com>
2023-02-11 01:58:47 +0600

    upload

--- resources/views/countdown.blade.php
+++ resources/views/countdown.blade.php
@@ -13,8 +13,35 @@ Author URL: http://webthemez.com
 License: Creative Commons Attribution 3.0 Unported
 License URL: http://creativecommons.org/licenses/by/3.0/
 */
-@import url(http://fonts.googleapis.com/css?family=Economica:400,700);
-@import url(http://fonts.googleapis.com/css?family=Lobster+Two:700);
+/* @import url(http://fonts.googleapis.com/css?family=Economica:400,700); */
+
+/* latin-ext */
+@font-face {
+  font-family: 'Economica';
+  font-style: normal;
+  font-weight: 400;
+  src: url({{ asset('fonts/Qw3fZQZaHCLgIWa29ZBbOMIBMl0.woff2') }}) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Economica';
+  font-style: normal;
+  font-weight: 400;
+  src: url({{ asset('fonts/Qw3fZQZaHCLgIWa29ZBbNsIB.woff2') }}) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+
+/* @import url(http://fonts.googleapis.com/css?family=Lobster+Two:700); */
+/* latin */
+@font-face {
+  font-family: 'Lobster Two';
+  font-style: normal;
+  font-weight: 700;
+  src: url({{ asset('fonts/BngRUXZGTXPUvIoyV6yN5-92w7CGwR0.woff2') }}) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
 body {
 	color:#FFF;
 	font-family: 'Economica', sans-serif;
