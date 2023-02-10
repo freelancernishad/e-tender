b3937c9ad26533811b3d15d58be6ada2ee837f0f
Softlabltd.com <freelancernishad123@gmail.com>
2023-02-11 01:51:30 +0600

    up

--- resources/views/countdown.blade.php
+++ resources/views/countdown.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>অনলাইন দরপত্র দাখিল সিস্টেমে স্বাগতম</title>
     <style>
 /*
 Author: WebThemez
